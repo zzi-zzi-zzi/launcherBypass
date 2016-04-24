34003f1c325db5f5fb15267408675e45995d67a1
Adam Smith <zzi@wolf.gg>
2016-04-24 19:45:49 -0400

    removed the ionic zlib Dependency.

--- launcherBypass/Utils/Crypto.cs
+++ launcherBypass/Utils/Crypto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,44 @@ namespace launcherBypass.Utils
 {
     class Crypto
     {
+
+        /// <summary>
+        /// Creates a valid zlib stream that the game will take
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        internal static byte[] ZlibCompress(byte[] data)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                // zlib header
+                outStream.WriteByte(0x58);
+                outStream.WriteByte(0x85);
+
+                // zlib body
+                using (var compressor = new DeflateStream(outStream, CompressionMode.Compress, true))
+                    compressor.Write(data, 0, data.Length);
+
+                // zlib checksum - a naive implementation of adler-32 checksum
+                const uint A32Mod = 65521;
+                uint s1 = 1, s2 = 0;
+                foreach (byte b in data)
+                {
+                    s1 = (s1 + b) % A32Mod;
+                    s2 = (s2 + s1) % A32Mod;
+                }
+
+                int adler32 = unchecked((int)((s2 << 16) + s1));
+                outStream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(adler32)), 0, sizeof(uint));
+
+                // zlib compatible compressed query
+                var bytes = outStream.ToArray();
+                outStream.Close();
+
+                return bytes;
+            }
+        }
+
         /// <summary>
         /// decrypts our data.
         /// </summary>
@@ -20,15 +59,26 @@ namespace launcherBypass.Utils
         {
             using (ICryptoTransform decryptor = _aes.CreateDecryptor(_aes.Key, _aes.IV))
             {
+                byte[] data;
                 using (MemoryStream ms = new MemoryStream())
                 {
                     using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
                     {
                         cs.Write(buffer, 0, buffer.Length);
                     }
-                    return Ionic.Zlib.ZlibStream.UncompressBuffer(ms.ToArray());
+                    data = ms.ToArray();
                 }
-            }// decryptor
+                using (MemoryStream ms = new MemoryStream(data))
+                {
+                    MemoryStream results = new MemoryStream();
+                    ms.Seek(2, SeekOrigin.Begin);
+                    using (DeflateStream z = new DeflateStream(ms, CompressionMode.Decompress))
+                    {
+                        z.CopyTo(results);
+                    }
+                    return results.ToArray();
+                }
+            }
         }
 
         /// <summary>
@@ -38,7 +88,7 @@ namespace launcherBypass.Utils
         /// <returns></returns>
         internal static byte[] Encrypt(byte[] buffer, AesManaged _aes, out int compressedSize)
         {
-            buffer = Ionic.Zlib.ZlibStream.CompressBuffer(buffer);
+            buffer = ZlibCompress(buffer);
             compressedSize =(int)buffer.Length; //because we want zlib not deflate
             using (ICryptoTransform encryptor = _aes.CreateEncryptor())
             {
